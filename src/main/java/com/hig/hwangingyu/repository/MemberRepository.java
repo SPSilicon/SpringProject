37a2369917ddb30a824cee3d923186ff617e478d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-09 15:58:08 +0900

    asd

--- src/main/java/com/hig/hwangingyu/repository/MemberRepository.java
+++ src/main/java/com/hig/hwangingyu/repository/MemberRepository.java
@@ -3,10 +3,8 @@ package com.hig.hwangingyu.repository;
 import java.util.Optional;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.hig.hwangingyu.domain.Member;
-
 
 public interface MemberRepository {
     void register(UserDetails member);
