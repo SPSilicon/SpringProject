b8ba865607de9909d01cd924995365ba299fa6d4
hig <dlsrb0722@naver.com>
2023-02-07 15:49:23 +0900

    add propertySource

--- src/main/java/com/hig/hwangingyu/HwangingyuApplication.java
+++ src/main/java/com/hig/hwangingyu/HwangingyuApplication.java
@@ -2,8 +2,10 @@ package com.hig.hwangingyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/application.properties")
 public class HwangingyuApplication {
 
 	public static void main(String[] args) {
