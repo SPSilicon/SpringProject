5425c9ee4dc7122331b2199b630441840beb138f
hig <dlsrb0722@naver.com>
2023-02-07 16:04:04 +0900

    secret moved to properties

--- src/main/java/com/hig/hwangingyu/utils/JWTProvider.java
+++ src/main/java/com/hig/hwangingyu/utils/JWTProvider.java
@@ -1,41 +1,62 @@
 package com.hig.hwangingyu.utils;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.Cookie;
+
+import org.springframework.beans.factory.annotation.Value;
+
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
+
 public class JWTProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
     
-    static public final String secret ="x#=o!FuR2RV9d5+Kj=koGMZwiRjuU2qcZX0zWA~^UVs)14UL0oU}sYv~*--=r-YeCh3nvCK+8.LetWEq!PZeM8BaF+i2D,x1~r?@";
-    static private Algorithm algorithm = Algorithm.HMAC256(secret);
-    
-    public static String createJWT(String id,List<String> auth) {
+    public String encode(String id,List<String> auth) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
         String token = JWT.create()
         .withClaim("username", id)
-        .withExpiresAt(java.sql.Timestamp.valueOf(LocalDateTime.now().plusMinutes(5)))
+        .withExpiresAt(java.sql.Timestamp.valueOf(LocalDateTime.now().plusMinutes(30)))
         .withClaim("auth",auth)
         .sign(algorithm);
 
         return token;
     }
 
-
-    public static DecodedJWT verify(String JWTtoken) {
-        JWTVerifier verifier = JWT.require(algorithm)
-        .withClaimPresence("username")
+    public Optional<DecodedJWT> decode(String JWTtoken) {
+        //JWTVerifier verifier = JWT.require(algorithm)
+        //.withClaimPresence("username")
         //.withClaimPresence("password") 비밀번호는 없음
-        .withClaimPresence("auth")
-        .build();
+        //.withClaimPresence("auth")
+        //.build();
 
-        return verifier.verify(JWTtoken);
+        DecodedJWT ret = JWT.decode(JWTtoken);
+        if(ret.getExpiresAt().before(new Date())) {
+            return Optional.empty();
+        }
+
+        
+        return Optional.ofNullable(ret);
     }
 
-    
-    public static String getSecret() {
-        return secret;
+    public Optional<DecodedJWT> getJWTfromCookies(Cookie[] cookies) {
+
+        Optional<DecodedJWT> ret = Optional.empty();
+        if(cookies!= null) {
+                for(Cookie c : cookies) {
+                    if(c.getName().compareTo("AUTHORIZATION")==0) {
+                        ret = decode(c.getValue());
+                    }
+            }
+        }
+        return ret;
     }
 }
