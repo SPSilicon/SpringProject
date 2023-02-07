9ab7077a0d25b8b1f91ced74225a7efa5ae9aa20
hig <dlsrb0722@naver.com>
2023-02-07 15:49:35 +0900

    Bean Update

--- src/main/java/com/hig/hwangingyu/config/Config.java
+++ src/main/java/com/hig/hwangingyu/config/Config.java
@@ -4,14 +4,23 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.hig.hwangingyu.filter.JwtAuthenticationFilter;
+import com.hig.hwangingyu.handler.LoginSuccessHandler;
+import com.hig.hwangingyu.handler.SocketWebmHandler;
 import com.hig.hwangingyu.repository.ArticleJdbcRepository;
 import com.hig.hwangingyu.repository.ArticleRepository;
 import com.hig.hwangingyu.repository.MemberRepository;
-import com.hig.hwangingyu.repository.MemberjdbcRepository;
+import com.hig.hwangingyu.repository.StreamRepository;
+import com.hig.hwangingyu.repository.StreamJdbcRepository;
+import com.hig.hwangingyu.repository.MemberJdbcRepository;
 import com.hig.hwangingyu.service.MemberService;
-
+import com.hig.hwangingyu.service.StreamService;
+import com.hig.hwangingyu.service.CustomOAuth2UserService;
 import com.hig.hwangingyu.service.ArticleService;
+import com.hig.hwangingyu.utils.JWTProvider;
 
 @Configuration
 public class Config {
@@ -21,12 +30,14 @@ public class Config {
     public Config(DataSource dataSource) { this.dataSource = dataSource;}
 
 
-
     @Bean
     public ArticleRepository articleRepository() {return new ArticleJdbcRepository(dataSource);}
 
     @Bean
-    public MemberRepository memberRepository() {return new MemberjdbcRepository(dataSource);}
+    public MemberRepository memberRepository() {return new MemberJdbcRepository(dataSource);}
+
+    @Bean
+    public StreamRepository streamRepository() { return new StreamJdbcRepository(dataSource); }
 
     @Bean
     public ArticleService articleService() { return new ArticleService(articleRepository());}
@@ -34,6 +45,25 @@ public class Config {
     @Bean
     public MemberService memberService() { return new MemberService(memberRepository());}
 
+    @Bean
+    public CustomOAuth2UserService customOAuth2UserService() { return new CustomOAuth2UserService(memberRepository(), passwordEncoder());}
 
+    @Bean
+    public StreamService streamService() { return new StreamService(streamRepository()); }
+
+    @Bean
+    public SocketWebmHandler socketWebmHandler() { return new SocketWebmHandler(streamService()); }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public JWTProvider jwtProvider() {return new JWTProvider(); }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() { return new LoginSuccessHandler(jwtProvider()); }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {return new JwtAuthenticationFilter(jwtProvider());}
 
 }
