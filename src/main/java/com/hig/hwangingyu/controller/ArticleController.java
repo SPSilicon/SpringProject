bd64370f25f3ba9062ae982110aee9fcb673f7f4
hig <dlsrb0722@naver.com>
2023-02-07 15:55:45 +0900

    update bean and jwt

--- src/main/java/com/hig/hwangingyu/controller/ArticleController.java
+++ src/main/java/com/hig/hwangingyu/controller/ArticleController.java
@@ -4,6 +4,7 @@ package com.hig.hwangingyu.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,24 +20,28 @@ import com.hig.hwangingyu.utils.JWTProvider;
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
+    private final JWTProvider jwtProvider;
 
    
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, JWTProvider jwtProvider) {
         this.articleService = articleService;
+        this.jwtProvider  = jwtProvider;
     }
 
     @PostMapping("post/add")
     public String add(Article articleForm, HttpServletRequest request) {
         System.out.println(articleForm.getBody());
-        for(Cookie c : request.getCookies()) {
-            if(c.getName().compareTo("AUTHORIZATION")==0) {
-                DecodedJWT jwt = JWTProvider.verify(c.getValue());
-                articleForm.setAuthor(jwt.getClaim("username").asString());
-            }
+
+        Cookie[] cookies = request.getCookies();
+
+        Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+        if(jwt.isPresent()) {
+            articleForm.setAuthor(jwt.get().getClaim("username").asString());
         }
+
         System.out.println(articleForm.getAuthor());
         articleService.uploadArticle(articleForm);
-        return "redirect:/";
+        return "redirect:/home";
     }
 
     @GetMapping("post/add")
@@ -44,10 +49,84 @@ public class ArticleController {
         return "addPost.html";
     }
 
-    @GetMapping("post")
-    public String read(@RequestParam Long id, Model model) {
+    @GetMapping("post/update")
+    public String update(@RequestParam Long id, Model model, HttpServletRequest request) {
+
+        Article post = articleService.findById(id).orElse(null);
+        if(post!=null) {
+            String curUsername="";
+            Cookie[] cookies = request.getCookies();
+    
+            Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+            if(jwt.isPresent()) {
+                curUsername = jwt.get().getClaim("username").asString();
+                model.addAttribute("username",curUsername);
+            }
+
+            if(post.getAuthor().equals(curUsername)) {
+                return "updatePost.html";
+            }
+        }
+
+
+        return "redirect:/home";
+    }
+
+    @PostMapping("post/update")
+    public String update(Article articleForm, Model model, HttpServletRequest request) {
+
+
+        if(articleForm!=null) {
+            String curUsername="";
+            Cookie[] cookies = request.getCookies();
+    
+            Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+            if(jwt.isPresent()) {
+                curUsername = jwt.get().getClaim("username").asString();
+                model.addAttribute("username",curUsername);
+            }
+
+            if(articleForm.getAuthor().equals(curUsername)) {
+                articleService.updateArticle(articleForm);
+
+                return "redirect:/home/post?id="+articleForm.getId();
+            }
+        }
+
+        return "redirect:/home";
+    }
+    @GetMapping("post/delete")
+    public String delete(@RequestParam Long id, Model model, HttpServletRequest request) {
+
+        String curUsername="";
+        Cookie[] cookies = request.getCookies();
+
+        Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+        if(jwt.isPresent()) {
+            curUsername = jwt.get().getClaim("username").asString();
+            model.addAttribute("username",curUsername);
+        }
+
+        Article article = articleService.read(id).orElse(null);
+        if(article!=null) {
+            if(article.getAuthor().equals(curUsername)) {
+                articleService.deleleArticle(id);
+            }
+        }
+        
+        return "redirect:/home";
+    }
+    @GetMapping("home/post")
+    public String read(@RequestParam Long id, Model model, HttpServletRequest request) {
         Article article = articleService.read(id).get();
         System.out.println(article.getBody());
+        
+        Cookie[] cookies = request.getCookies();
+        Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+        if(jwt.isPresent()) {
+            model.addAttribute("username",jwt.get().getClaim("username").asString());
+        }
+
         model.addAttribute("article", articleService.read(id).get());
 
         return "article.html";
--- src/main/java/com/hig/hwangingyu/controller/HomeController.java
+++ src/main/java/com/hig/hwangingyu/controller/HomeController.java
@@ -2,6 +2,11 @@ package com.hig.hwangingyu.controller;
 
 
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -10,24 +15,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.hig.hwangingyu.domain.Article;
 
 import com.hig.hwangingyu.service.ArticleService;
+import com.hig.hwangingyu.utils.JWTProvider;
 
 @Controller
 public class HomeController {
     private final ArticleService articleService;
-
+    private final JWTProvider jwtProvider;
     
-    public HomeController(ArticleService articleService) {
+    public HomeController(ArticleService articleService, JWTProvider jwtProvider) {
         this.articleService = articleService;
+        this.jwtProvider = jwtProvider;
     }
-    @GetMapping("/")
-    public String first() {
+
+    @GetMapping("")
+    public String welcome() {
         return "redirect:/home/0";
     }
+    @GetMapping("/home")
+    public String first() {
+        
+        return "redirect:/home/0";
+    }
+
     @GetMapping("home/{pageNum}")
-    public String home(@PathVariable int pageNum, Model model) {
+    public String home(@PathVariable int pageNum, Model model, HttpServletRequest request) {
+        
+        String curUsername="";
+        Cookie[] cookies = request.getCookies();
+
+        Optional<DecodedJWT> jwt = jwtProvider.getJWTfromCookies(cookies);
+        if(jwt.isPresent()) {
+            curUsername = jwt.get().getClaim("username").asString();
+            model.addAttribute("username",curUsername);
+        }
 
         Pageable pageable = PageRequest.of(pageNum, 20);
         Page<Article> page = articleService.findAll(pageable);
