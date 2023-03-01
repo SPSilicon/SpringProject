4981a95dc50bd08d97bceb9f4ceb7ec4b5816822
hig <dlsrb0722@naver.com>
2023-03-02 00:28:10 +0900

    exceptionhanding

--- src/main/java/com/hig/hwangingyu/error/CustomExceptionHandler.java
+++ src/main/java/com/hig/hwangingyu/error/CustomExceptionHandler.java
@@ -2,19 +2,25 @@ package com.hig.hwangingyu.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @ControllerAdvice
 public class CustomExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler({IllegalStateException.class})
-    public String handleBadRequestException(WebRequest request, RuntimeException ex, Model model) {
-        //TODO 메인화면에서 경고창뜨게 만들기
+    public String handleBadRequestException(WebRequest request, RuntimeException ex, RedirectAttributes redirectAttribute) {
+        //TODO 경고창 팝업 뜨게 만들기
+        
         log.info(ex.getMessage());
-        return "redirect:/home";
+        redirectAttribute.addFlashAttribute("error", ex.getMessage());
+        String target = request.getHeader("referer") == null? "home" : request.getHeader("Referer");
+        return "redirect:/"+target;
     }
 }
