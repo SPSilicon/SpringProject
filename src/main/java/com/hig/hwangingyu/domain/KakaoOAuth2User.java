21348065089c9114c5ab611567b4cc83a9ba2096
SPSilicon <dlsrb915@gmail.com>
2023-04-05 17:00:33 +0900

    add user auth

--- src/main/java/com/hig/hwangingyu/domain/KakaoOAuth2User.java
+++ src/main/java/com/hig/hwangingyu/domain/KakaoOAuth2User.java
@@ -1,16 +1,18 @@
 package com.hig.hwangingyu.domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
-
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 
 public class KakaoOAuth2User implements OAuth2User{
 
-	private final Collection<? extends GrantedAuthority> authorities;
+	private final List<GrantedAuthority> authorities;
 
 	private final Map<String, Object> attributes;
 
@@ -19,7 +21,14 @@ public class KakaoOAuth2User implements OAuth2User{
     
     public KakaoOAuth2User(OAuth2User oAuth2User) {
         this.attributes = oAuth2User.getAttributes();
-        this.authorities = oAuth2User.getAuthorities();
+        this.authorities = new ArrayList<>();
+        GrantedAuthority defaultAuth = new SimpleGrantedAuthority("ROLE_USER");
+        this.authorities.add(defaultAuth);
+        for(GrantedAuthority auth :oAuth2User.getAuthorities()) {
+            this.authorities.add(auth);
+        }
+        
+
         Object obj = oAuth2User.getAttribute("kakao_account");
         if(obj instanceof Map<?,?>) {
             this.kakaoAccount = ((Map<String, Object>)obj);
